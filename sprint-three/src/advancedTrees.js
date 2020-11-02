f62b865bb962fca458654be62b4a642ef19070e3
Alexander Nguyen <alexneeeeewin@icloud.com>
2020-11-01 16:51:52 -0800

    Should add seperate sprint files for advanced content on a new branch

---
+++ sprint-three/src/advancedTrees.js
@@ -0,0 +1,24 @@
+class Node {
+  constructor (value) {
+    this.value = value;
+    this.children = [];
+  }
+}
+
+class Tree {
+  constructor(value) {
+    this.root = new Node(value);
+  }
+
+  addChild (value) {
+
+  }
+
+  contains (value) {
+
+  }
+
+  traverse (cb) {
+
+  }
+}
